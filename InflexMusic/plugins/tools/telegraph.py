7ecca34ddde1d27453cc8d91503bd4b75fd38e85
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-23 20:53:45 +0400

    Update telegraph.py

--- InflexMusic/plugins/tools/telegraph.py
+++ InflexMusic/plugins/tools/telegraph.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from BrandrdXMusic import app
+from InflexMusic import app
 import requests
 
 
