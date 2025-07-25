610756c3115e46c9968def3b27dd043dfaf25884
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-25 15:39:42 +0400

    Update clonedb.py

--- InflexMusic/utils/database/clonedb.py
+++ InflexMusic/utils/database/clonedb.py
@@ -1,4 +1,4 @@
-from BrandrdXMusic.core.mongo import mongodb, pymongodb
+from İnflexMusic.core.mongo import mongodb, pymongodb
 from typing import Dict, List, Union
 
 cloneownerdb = mongodb.cloneownerdb
