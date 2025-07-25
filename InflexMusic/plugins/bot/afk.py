6157a50ef286e74852da1dd77d83c0c2a9d0802f
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-25 17:05:35 +0400

    Update afk.py

--- InflexMusic/plugins/bot/afk.py
+++ InflexMusic/plugins/bot/afk.py
@@ -2,9 +2,9 @@ import time, re
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
-from BrandrdXMusic import app
-from BrandrdXMusic.mongo.readable_time import get_readable_time
-from BrandrdXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
+from InflexMusic import app
+from InflexMusic.mongo.readable_time import get_readable_time
+from InflexMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
