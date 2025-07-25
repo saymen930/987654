6ae1554bf0ca2bba5d87c40151322ee32da2f157
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-26 01:06:16 +0400

    Update İnfo.py

--- InflexMusic/plugins/tools/İnfo.py
+++ InflexMusic/plugins/tools/İnfo.py
@@ -1,26 +1,20 @@
-from pyrogram import Client, filters
-from InflexMusic import app  # əgər `app` Client-dirsə
+from pyrogram import filters
+from InflexMusic import app  # Sənin musiqi botunun Client obyekti
 
 @app.on_message(filters.command("info") & (filters.group | filters.private))
 async def info(client, message):
-    if message.reply_to_message:
-        user = message.reply_to_message.from_user
-    else:
-        user = message.from_user
+    user = message.reply_to_message.from_user if message.reply_to_message else message.from_user
 
     name = user.first_name or "Yoxdur"
     username = f"@{user.username}" if user.username else "Yoxdur"
     user_id = user.id
-    profile_link = f'<a href="tg://user?id={user_id}">{name}</a>'
+    profile_link = f"[{name}](tg://user?id={user_id})"
 
     text = (
-        f"👾 <b>İstifadəçi Adı:</b> {name}\n"
-        f"🔮 <b>Username:</b> {username}\n"
-        f"🆔 <b>ID nömrəsi:</b> <code>{user_id}</code>\n"
-        f"🥷 <b>Profil:</b> {profile_link}"
+        f"👾 *İstifadəçi Adı:* {name}\n"
+        f"🔮 *Username:* {username}\n"
+        f"🆔 *ID nömrəsi:* `{user_id}`\n"
+        f"🥷 *Profil:* {profile_link}"
     )
 
-    await message.reply(
-        text,
-        parse_mode="HTML"
-    )
+    await message.reply(text, parse_mode="Markdown")
