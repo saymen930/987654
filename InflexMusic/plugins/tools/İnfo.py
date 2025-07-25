792ee98f3f18b748939edf3c02322f7d33a310b2
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-26 02:01:41 +0400

    Update İnfo.py

--- InflexMusic/plugins/tools/İnfo.py
+++ InflexMusic/plugins/tools/İnfo.py
@@ -6,7 +6,7 @@
 import os, asyncio, time, shlex, requests, pyrogram
 from helpers.extract_user import extract_user, last_online
 from datetime import datetime
-from İnflexMusic import InflexMusic as app
+from InflexMusic import InflexMusic as app
 from pyrogram import filters, Client
 
 
