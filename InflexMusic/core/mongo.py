5f0c716077bc7e7ae1da4fbe9067c5b678e5e23a
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-25 16:11:41 +0400

    Update mongo.py

--- InflexMusic/core/mongo.py
+++ InflexMusic/core/mongo.py
@@ -1,7 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
 from config import MONGO_DB_URI
-
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
@@ -10,6 +8,6 @@ try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.ByMidoffMusicBot
     LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+except Exception as e:
+    LOGGER(__name__).error(f"Failed to connect to your Mongo Database: {e}")
     exit()
