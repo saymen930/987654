de4447e1fd87ee26c040dc7130ec92f89c88f269
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-26 17:45:35 +0400

    Update id🆔.py

--- InflexMusic/plugins/tools/id🆔.py
+++ InflexMusic/plugins/tools/id🆔.py
@@ -3,18 +3,26 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @app.on_message(filters.command("id"))
-def ids(_, message):
+async def ids(_, message):
     reply = message.reply_to_message
     button = InlineKeyboardButton("✯ Bağla ✯", callback_data="close")
     markup = InlineKeyboardMarkup([[button]])
 
-    if reply:
-        message.reply_text(
+    if reply and reply.from_user:
+        await message.reply_text(
             f"👤 İstifadəçi: {reply.from_user.first_name}\n🆔 ID: `{reply.from_user.id}`",
-            reply_markup=markup
+            reply_markup=markup,
+            quote=True
         )
     else:
-        message.reply(
+        await message.reply_text(
            f"💬 Bu qrupun ID-si: `{message.chat.id}`",
-           reply_markup=markup
+           reply_markup=markup,
+           quote=True
         )
+
+# Callback query handler - "close" düyməsinə cavab
+@app.on_callback_query(filters.regex("close"))
+async def close_callback(client, callback_query):
+    await callback_query.message.delete()
+    await callback_query.answer()  # Hər zaman cavab vermək lazımdır
