9ef76ebd2ac6a17ddc10e4928958c0019a6dea90
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-26 15:35:41 +0400

    Create id🆔.py

---
+++ InflexMusic/plugins/tools/id🆔.py
@@ -0,0 +1,20 @@
+from BrandrdXMusic import app
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@app.on_message(filters.command("id"))
+def ids(_, message):
+    reply = message.reply_to_message
+    button = InlineKeyboardButton("✯ Bağla ✯", callback_data="close")
+    markup = InlineKeyboardMarkup([[button]])
+
+    if reply:
+        message.reply_text(
+            f"👤 İstifadəçi: {reply.from_user.first_name}\n🆔 ID: `{reply.from_user.id}`",
+            reply_markup=markup
+        )
+    else:
+        message.reply(
+           f"💬 Bu qrupun ID-si: `{message.chat.id}`",
+           reply_markup=markup
+        )
