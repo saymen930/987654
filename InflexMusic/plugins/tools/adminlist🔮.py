1ee5037d2a21d3e4efa3e9b0fb18b3b476d79dff
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-26 14:08:27 +0400

    Update adminlist🔮.py

--- InflexMusic/plugins/tools/adminlist🔮.py
+++ InflexMusic/plugins/tools/adminlist🔮.py
@@ -1,15 +1,16 @@
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.types import Message
-from InflexMusic import app
-
+from InflexMusic import app  # app = Inflex()
 
 @app.on_message(filters.command("adminlist") & filters.group)
-async def adminlist(client: Client, message: Message):
+async def adminlist(_, message: Message):
     chat_id = message.chat.id
     try:
-        admins = await client.get_chat_administrators(chat_id)
+        admins = await app.get_chat_administrators(chat_id)
     except Exception as e:
-        return await message.reply(f"❌ Admin siyahısını əldə etmək mümkün olmadı.\n\n**Xəta:** `{str(e)}`")
+        return await message.reply(
+            f"❌ Admin siyahısını əldə etmək mümkün olmadı.\n\n**Xəta:** `{str(e)}`"
+        )
 
     if not admins:
         return await message.reply("⚠️ Admin siyahısı boşdur.")
