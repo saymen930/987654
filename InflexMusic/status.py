e469cb90898d1b17dbbd6a58acc99dcda904d0a6
Ofurlayan.pesidi <117319718+Kolge04@users.noreply.github.com>
2025-07-26 22:46:04 +0400

    Create status.py

---
+++ InflexMusic/status.py
@@ -0,0 +1,21 @@
+import functools
+ 
+ 
+def is_admin(func):
+    @functools.wraps(func)
+    async def a_c(event):
+        is_admin = False
+        if not event.is_private:
+            try:
+                _s = await event.client.get_permissions(event.chat_id, event.sender_id)
+                if _s.is_admin:
+                    is_admin = True
+            except:
+                is_admin = False
+        if is_admin:
+            await func(event, _s)
+        else:
+            await event.reply("❌ Bu Əmr Yanlız Adminlər Üçün Keçərlidir")
+ 
+    return a_c
+  
