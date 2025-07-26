539e942a237509be57ff6c899e6feb9fbc57dae7
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-26 16:07:42 +0400

    Update nosoyus🔞.py

--- InflexMusic/plugins/Elv/nosoyus🔞.py
+++ InflexMusic/plugins/Elv/nosoyus🔞.py
@@ -1,14 +1,14 @@
+from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 from InflexMusic import app
+
 # Söyüş kökləri və şəkilçilər
 söyüşlər = ["sikdir", "qehbe", "qəhbə", "anavı", "cındır", "blət","blet", "sikim", "oğraş", "dalbayov","osduraq", "boynu", "nın", "vı","va", "s2m", "Dalbek", "dalben","qandon", "dişi", "fayşə", "faişə","qehbe", "təpdir", "siydir", "siyim","qehbe", "qəhbə", "vz", "vzqırt","kiwi", "oğlancıq", "bacını", "xirtəyini","boşalıram", "verirəm", "real", "virtual","çal", "banan", "gala", "uç","hamilə", "donbal", "Dombale", "Dombal" ]
 söyüş_şəkilçilər = ["nın", "vı", "vu", "sər", "m", "un", "ni", "nı"]
 
-# Aktiv olan qruplar üçün saxlama
 aktiv_qruplar = {}
 
-# Söyüş yoxlama funksiyası
 def söyüş_var(metin):
     metin = metin.lower().replace(" ", "")
     for söz in söyüşlər:
@@ -19,11 +19,10 @@ def söyüş_var(metin):
                 return True
     return False
 
-# No söyüş funksiyasını aç/bağla
 @app.on_message(filters.command("noargo") & filters.group)
 async def toggle_no_söyüş(_, message: Message):
     if len(message.command) < 2:
-        await message.reply("İstifadə: /noargo  yes və ya /noargo no")
+        await message.reply("İstifadə: /noargo yes və ya /noargo no")
         return
 
     cmd = message.command[1].lower()
@@ -36,14 +35,9 @@ async def toggle_no_söyüş(_, message: Message):
     else:
         await message.reply("Yanlış seçim, /noargo yes və ya /noargo no yaz.")
 
-# Qrup mesajlarını yoxlayır
-@app.on_message(filters.group & ~filters.command("nosöyüş"))
+@app.on_message(filters.group & ~filters.command("noargo"))
 async def söyüş_yoxla(_, message: Message):
     if aktiv_qruplar.get(message.chat.id):
         if message.text and söyüş_var(message.text):
             await message.delete()
-            await message.reply(f"Hörmətli {message.from_user.mention} Zəhmət olmasa Etikdadan kənar Argo kəlmələr istifadə etməyək,Əks halda ban oluna bilərsiniz😊 ")
-
-
-    # Mesajı silmək
-    await message.delete()
+            await message.reply(f"Hörmətli {message.from_user.mention} Zəhmət olmasa Etikdadan kənar Argo kəlmələr istifadə etməyək, Əks halda ban oluna bilərsiniz😊 ")
