7977b15e4abd58e8b93b6c798c066f04d70af5b4
Ofurlayan.pesidi <117319718+Kolge04@users.noreply.github.com>
2025-07-27 20:05:41 +0400

    Update Lock🔐.py

--- InflexMusic/plugins/tag/Lock🔐.py
+++ InflexMusic/plugins/tag/Lock🔐.py
@@ -1,67 +1,107 @@
 from telethon import events, Button, types
 from InflexMusic.status import *
-from InflexMusic.core.bot import xaos as Zaid
+from InflexMusic.core.bot import xaos as client
 import config
+from Jason.lock import LOCK_TEXT
 
-@Zaid.on(events.NewMessage(pattern="^[!/.]lock ?(.*)"))
+
+
+
+
+@client.on(events.NewMessage(pattern="^[!/.]lock ?(.*)"))
 @is_admin
 async def lock(event, perm):
     if config.MANAGEMENT_MODE == "ENABLE":
         return
     if not perm.change_info:
-      await event.reply("Bu Əmri İsdifadə Edmək Üçun Lazım Olan Adminlik Haqqın Yoxdur!")
-      return
-    input_str = event.pattern_match.group(1)
-    if not input_str:
-       await event.reply("Kilidləmək Üçün Növü Müəyyən Etmədiniz..")
-       return
-    elif "all" in input_str:
-       await Zaid.edit_permissions(event.chat_id,
-          send_messages=False, 
-          send_media=False,
-          send_stickers=False,
-          send_gifs=False,
-          send_games=False,
-          send_inline=False,
-          send_polls=False,
-          embed_link_previews=False)
-       await event.reply(f"🔐 Chat Bağlandı.")
+        await event.reply("Bu Əmri İsdifadə Edmək Üçün Lazım Olan Adminlik Haqqın Yoxdur!")
+        return
 
-@Zaid.on(events.NewMessage(pattern="^[!./]unlock ?(.*)"))
+    input_str = event.pattern_match.group(1).lower()
+    if not input_str:
+        await event.reply("Kilidləmək Üçün Növü Müəyyən Etmədiniz..\n/locktypes")
+        return
+    
+    if "all" in input_str:
+        await client.edit_permissions(
+            event.chat_id,
+            send_messages=False,
+            send_media=False,
+            send_stickers=False,
+            send_gifs=False,
+            send_games=False,
+            send_inline=False,
+            send_polls=False,
+            
+            embed_link_previews=False
+            
+        )
+        await event.reply("🔐 Chat Bağlandı.")
+    elif "gif" in input_str:
+        await client.edit_permissions(event.chat_id, send_gifs=False)
+        await event.reply("🔐 Gif göndərmək bağlandı.")
+    elif "sticker" in input_str:
+        await client.edit_permissions(event.chat_id, send_stickers=False)
+        await event.reply("🔐 Sticker göndərmək bağlandı.")
+        
+    elif "media" in input_str:
+        await client.edit_permissions(event.chat_id, send_media=False)
+        await event.reply("🔐 Media göndərmək bağlandı.")
+    
+  
+
+
+@client.on(events.NewMessage(pattern="^[!/.]unlock ?(.*)"))
 @is_admin
 async def unlock(event, perm):
     if config.MANAGEMENT_MODE == "ENABLE":
         return
     if not perm.change_info:
-      await event.reply("Bu Əmri İsdifadə Edmək Üçun Lazım Olan Adminlik Haqqın Yoxdur!")
-      return
-    input_str = event.pattern_match.group(1)
+        await event.reply("Bu Əmri İsdifadə Edmək Üçün Lazım Olan Adminlik Haqqın Yoxdur!")
+        return
+
+    input_str = event.pattern_match.group(1).lower()
     if not input_str:
-       await event.reply("Kilidi Açmaq Üçün Növü Müəyyən Etmədiniz.‌‌.")
-       return
-    elif "all" in input_str:
-       await Zaid.edit_permissions(event.chat_id,
-          send_messages=True, 
-          send_media=True,
-          send_stickers=True,
-          send_gifs=True,
-          send_games=True,
-          send_inline=True,
-          send_polls=True,
-          embed_link_previews=True)
-       await event.reply("🔓 Chat Açıldı.")
+        await event.reply("Kilidi Açmaq Üçün Növü Müəyyən Etmədiniz.‌‌.\n/locktypes")
+        return
+    
+    if "all" in input_str:
+        await client.edit_permissions(
+            event.chat_id,
+            send_messages=True,
+            send_media=True,
+            send_stickers=True,
+            send_gifs=True,
+            send_games=True,
+            send_inline=True,
+            send_polls=True,
+            
+            embed_link_previews=True
+        )
+        await event.reply("🔓 Chat Açıldı.")
+    elif "gif" in input_str:
+        await client.edit_permissions(event.chat_id, send_gifs=True)
+        await event.reply("🔓 Gif göndərmək açıldı.")
+    elif "sticker" in input_str:
+        await client.edit_permissions(event.chat_id, send_stickers=True)
+        await event.reply("🔓 Sticker və emoji göndərmək açıldı.")
+        
+    elif "media" in input_str:
+        await client.edit_permissions(event.chat_id, send_media=True)
+        await event.reply("🔓 Media göndərmək açıldı.")
 
 
 
 
-LOCK_TEXT = """
-**Lock:**
+
+
+
  
-‣ all
-"""
- 
-@Zaid.on(events.NewMessage(pattern="^[!?/]locktypes"))
+@client.on(events.NewMessage(pattern="^[!?/]locktypes"))
 async def locktypes(event):
     await event.reply(LOCK_TEXT)
- 
+    
+
+
+
  
