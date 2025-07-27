69ba16a8776f89265f1e2e36e81334147141f0dc
Ofurlayan.pesidi <117319718+Kolge04@users.noreply.github.com>
2025-07-27 11:09:10 +0400

    Create sahibPin👮.py

---
+++ InflexMusic/plugins/tag/sahibPin👮.py
@@ -0,0 +1,34 @@
+import asyncio, random
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+import config
+from InflexMusic.core.bot import xaos as client
+
+# Bir nəfər yerinə bir neçə ID:
+
+def is_owner(user_id: int) -> bool:
+    return user_id in config.OWNER_IDS
+
+@client.on(events.NewMessage(pattern=r"^[/.!]pins ?(.*)"))
+async def pin(event):
+    if not is_owner(event.sender_id):
+        return await event.reply(f"Sən {config.BOT_NAME} bota sahib deyilsən!\n⛔ Pinləməyə çalışma.")
+
+    if not event.reply_to_msg_id:
+        return await event.reply("🗨 Zəhmət olmasa bir mesaja yanıt verin.")
+
+    await event.client.pin_message(event.chat_id, event.reply_to_msg_id, notify=True)
+    await event.reply("📌 Mesaj pinləndi!")
+
+@client.on(events.NewMessage(pattern=r"^[/.!]unpins ?(.*)"))
+async def unpin(event):
+    if not is_owner(event.sender_id):
+        return await event.reply(f"Sən {config.BOT_NAME} bota sahib deyilsən!\n⛔ Unpinləməyə çalışmayın.")
+
+    if not event.reply_to_msg_id:
+        return await event.reply("🗨 Zəhmət olmasa pinlənmiş mesaja yanıt verin.")
+
+    await event.client.unpin_message(event.chat_id)
+    await event.reply("📍 Pin qaldırıldı.")
