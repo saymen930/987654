3b0e05fb52f31ae289e2b3a5fa4848019929707e
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-29 00:26:55 +0400

    Update start.py

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -32,12 +32,9 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="BASICBOTS"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_GROUP),
-        ],[
             InlineKeyboardButton(text=_["S_B_5"], url=config.OWNER_NAME),
         ],
     ]
