d262c9a2c63a52336fcb2222bde0cd2783070903
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-29 22:13:29 +0400

    Update misc.py

--- InflexMusic/misc.py
+++ InflexMusic/misc.py
@@ -72,3 +72,5 @@ def heroku():
                 LOGGER(__name__).warning(
                     f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
                 )
+
+#repo icazesiz goturme
