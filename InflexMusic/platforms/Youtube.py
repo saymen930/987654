135fae85632106dd039469bb928304610817d844
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-29 22:15:23 +0400

    Update Youtube.py

--- InflexMusic/platforms/Youtube.py
+++ InflexMusic/platforms/Youtube.py
@@ -408,3 +408,6 @@ class YouTubeAPI:
             direct = True
             downloaded_file = await loop.run_in_executor(None, audio_dl)
         return downloaded_file, direct
+
+
+#repo icazesiz goturme
