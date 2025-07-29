36d0461b482bd2a89984bbb2f89fbfad0f6c08bb
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-29 22:20:31 +0400

    Update Apple.py

--- InflexMusic/platforms/Apple.py
+++ InflexMusic/platforms/Apple.py
@@ -67,3 +67,5 @@ class AppleAPI:
                 xx = ((item["content"]).split("album/")[1]).split("/")[0]
             results.append(xx)
         return results, playlist_id
+        
+#repo icazesiz goturme
