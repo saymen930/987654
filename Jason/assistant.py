a73eb2fefd978269899796b70d234ce6db3c8c1f
Ofurlayan.pesidi <117319718+Kolge04@users.noreply.github.com>
2025-07-30 11:08:12 +0400

    Create assistant.py

---
+++ Jason/assistant.py
@@ -0,0 +1,13 @@
+import config
+
+O_MSG = "⚠️ Bu əmri yalnız qruplarda istifadə edə bilər və ya asistanta daxil olaraq .music yazılmalıdır."
+
+P_MSG = "👤 Asistant bu qrupda deyil. Zəhmət olmasa onu qrupa əlavə edin."
+
+
+P_BAN = "🔇 Asistant bu qrupda səsizə alınıb. Zəhmət olmasa /unmute əmri ilə səsin açın.\n\n🆔 `5871660730`\n🌐 @sesizKOLGE"
+
+buttons = [[Button.url("🎧 Asistant Hesabı »", f"https://t.me/{config.ASİSTANT_USERNAME}")]]
+
+#(button = [[Button.url("🎧 Asistant Hesabı »", f"https://t.me/{userbot_username}")]]                
+
