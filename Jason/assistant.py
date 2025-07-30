2a1cda5d33788b7d4a6ecbf8ef76e32563625401
Ofurlayan.pesidi <117319718+Kolge04@users.noreply.github.com>
2025-07-30 11:15:27 +0400

    Update assistant.py

--- Jason/assistant.py
+++ Jason/assistant.py
@@ -7,7 +7,7 @@ P_MSG = "👤 Asistant bu qrupda deyil. Zəhmət olmasa onu qrupa əlavə edin."
 
 P_BAN = "🔇 Asistant bu qrupda səsizə alınıb. Zəhmət olmasa /unmute əmri ilə səsin açın.\n\n🆔 `5871660730`\n🌐 @sesizKOLGE"
 
-buttons = [[Button.url("🎧 Asistant Hesabı »", f"https://t.me/{config.ASİSTANT_USERNAME}")]]
+buttons = [[Button.url("🎧 Asistant Hesabı »", f"https://t.me/{config.userbot_username}")]]
 
 #(button = [[Button.url("🎧 Asistant Hesabı »", f"https://t.me/{userbot_username}")]]                
 
