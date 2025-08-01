894d6f2bb5ec9fc38ef88a0cb04684978bb51e3c
eko777e <replitserver445@gmail.com>
2025-08-01 13:45:26 +0400

    Update userbot.py

--- InflexMusic/core/userbot.py
+++ InflexMusic/core/userbot.py
@@ -1,21 +1,53 @@
 from pyrogram import Client
+
 import config
+
 from ..logging import LOGGER
 
 assistants = []
 assistantids = []
 
-class Userbot:
+
+class Userbot(Client):
     def __init__(self):
-        self.one = Client("InflexAss1", config.API_ID, config.API_HASH, session_string=str(config.STRING1), no_updates=True)
-        self.two = Client("InflexAss2", config.API_ID, config.API_HASH, session_string=str(config.STRING2), no_updates=True)
-        self.three = Client("InflexAss3", config.API_ID, config.API_HASH, session_string=str(config.STRING3), no_updates=True)
-        self.four = Client("InflexAss4", config.API_ID, config.API_HASH, session_string=str(config.STRING4), no_updates=True)
-        self.five = Client("InflexAss5", config.API_ID, config.API_HASH, session_string=str(config.STRING5), no_updates=True)
+        self.one = Client(
+            name="InflexAss1",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1),
+            no_updates=True,
+        )
+        self.two = Client(
+            name="InflexAss2",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2),
+            no_updates=True,
+        )
+        self.three = Client(
+            name="InflexAss3",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING3),
+            no_updates=True,
+        )
+        self.four = Client(
+            name="InflexAss4",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING4),
+            no_updates=True,
+        )
+        self.five = Client(
+            name="InflexAss5",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING5),
+            no_updates=True,
+        )
 
     async def start(self):
-        LOGGER(__name__).info("Starting Assistants...")
-
+        LOGGER(__name__).info(f"Starting Assistants...")
         if config.STRING1:
             await self.one.start()
             try:
@@ -27,97 +59,102 @@ class Userbot:
             try:
                 await self.one.send_message(config.LOG_GROUP_ID, "Assistent aktiv edildi")
             except:
-                LOGGER(__name__).error("Assistant 1 log qrupuna çata bilmir. Admin et.")
+                LOGGER(__name__).error(
+                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                )
                 exit()
-            me = await self.one.get_me()
-            self.one.id = me.id
-            self.one.name = me.mention
-            self.one.username = me.username
-            assistantids.append(me.id)
-            LOGGER(__name__).info(f"Assistant 1: {me.first_name}")
+            self.one.id = self.one.me.id
+            self.one.name = self.one.me.mention
+            self.one.username = self.one.me.username
+            assistantids.append(self.one.id)
+            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("basicbots")
-                await self.two.join_chat("gizliplanet")
+                await self.one.join_chat("basicbots")
+                await self.one.join_chat("gizliplanet")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(config.LOG_GROUP_ID, "Assistent aktiv edildi")
             except:
-                LOGGER(__name__).error("Assistant 2 log qrupuna çata bilmir.")
+                LOGGER(__name__).error(
+                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                )
                 exit()
-            me = await self.two.get_me()
-            self.two.id = me.id
-            self.two.name = me.mention
-            self.two.username = me.username
-            assistantids.append(me.id)
-            LOGGER(__name__).info(f"Assistant 2: {me.first_name}")
+            self.two.id = self.two.me.id
+            self.two.name = self.two.me.mention
+            self.two.username = self.two.me.username
+            assistantids.append(self.two.id)
+            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
 
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("basicbots")
-                await self.three.join_chat("gizliplanet")
+                await self.one.join_chat("basicbots")
+                await self.one.join_chat("gizliplanet")
             except:
                 pass
             assistants.append(3)
             try:
                 await self.three.send_message(config.LOG_GROUP_ID, "Assistent aktiv edildi")
             except:
-                LOGGER(__name__).error("Assistant 3 log qrupuna çata bilmir.")
+                LOGGER(__name__).error(
+                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
                 exit()
-            me = await self.three.get_me()
-            self.three.id = me.id
-            self.three.name = me.mention
-            self.three.username = me.username
-            assistantids.append(me.id)
-            LOGGER(__name__).info(f"Assistant 3: {me.first_name}")
+            self.three.id = self.three.me.id
+            self.three.name = self.three.me.mention
+            self.three.username = self.three.me.username
+            assistantids.append(self.three.id)
+            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
 
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("basicbots")
-                await self.four.join_chat("gizliplanet")
+                await self.one.join_chat("basicbots")
+                await self.one.join_chat("gizliplanet")
             except:
                 pass
             assistants.append(4)
             try:
                 await self.four.send_message(config.LOG_GROUP_ID, "Assistent aktiv edildi")
             except:
-                LOGGER(__name__).error("Assistant 4 log qrupuna çata bilmir.")
+                LOGGER(__name__).error(
+                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
                 exit()
-            me = await self.four.get_me()
-            self.four.id = me.id
-            self.four.name = me.mention
-            self.four.username = me.username
-            assistantids.append(me.id)
-            LOGGER(__name__).info(f"Assistant 4: {me.first_name}")
+            self.four.id = self.four.me.id
+            self.four.name = self.four.me.mention
+            self.four.username = self.four.me.username
+            assistantids.append(self.four.id)
+            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
 
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("basicbots")
-                await self.five.join_chat("gizliplanet")
+                await self.one.join_chat("basicbots")
+                await self.one.join_chat("gizliplanet")
             except:
                 pass
             assistants.append(5)
             try:
                 await self.five.send_message(config.LOG_GROUP_ID, "Assistent aktiv edildi")
             except:
-                LOGGER(__name__).error("Assistant 5 log qrupuna çata bilmir.")
+                LOGGER(__name__).error(
+                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
                 exit()
-            me = await self.five.get_me()
-            self.five.id = me.id
-            self.five.name = me.mention
-            self.five.username = me.username
-            assistantids.append(me.id)
-            LOGGER(__name__).info(f"Assistant 5: {me.first_name}")
+            self.five.id = self.five.me.id
+            self.five.name = self.five.me.mention
+            self.five.username = self.five.me.username
+            assistantids.append(self.five.id)
+            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
 
     async def stop(self):
-        LOGGER(__name__).info("Stopping Assistants...")
+        LOGGER(__name__).info(f"Stopping Assistants...")
         try:
             if config.STRING1:
                 await self.one.stop()
