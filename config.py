cd5d05868b541d78435caaea535b476c3e900240
eko777e <replitserver445@gmail.com>
2025-10-30 12:38:48 +0400

    Update config.py

--- config.py
+++ config.py
@@ -97,36 +97,36 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = ["https://files.catbox.moe/mgvjm4.jpg"]
+START_IMG_URL = ["https://files.catbox.moe/2pwrhs.jpg"]
 
-PING_IMG_URL = "https://files.catbox.moe/mgvjm4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/mgvjm4.jpg"
+PING_IMG_URL = "https://files.catbox.moe/2pwrhs.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/2pwrhs.jpg"
 PLAYLIST_IMG_URL = getenv(
-    "PLAYLIST_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "PLAYLIST_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 TELEGRAM_AUDIO_URL = getenv(
-    "TELEGRAM_AUDIO_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "TELEGRAM_AUDIO_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 TELEGRAM_VIDEO_URL = getenv(
-    "TELEGRAM_VIDEO_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "TELEGRAM_VIDEO_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 STREAM_IMG_URL = getenv(
-    "STREAM_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "STREAM_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 SOUNCLOUD_IMG_URL = getenv(
-    "SOUNCLOUD_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "SOUNCLOUD_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 YOUTUBE_IMG_URL = getenv(
-    "YOUTUBE_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "YOUTUBE_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 SPOTIFY_ARTIST_IMG_URL = getenv(
-    "SPOTIFY_ARTIST_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "SPOTIFY_ARTIST_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 SPOTIFY_ALBUM_IMG_URL = getenv(
-    "SPOTIFY_ALBUM_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "SPOTIFY_ALBUM_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 SPOTIFY_PLAYLIST_IMG_URL = getenv(
-    "SPOTIFY_PLAYLIST_IMG_URL", "https://files.catbox.moe/mgvjm4.jpg"
+    "SPOTIFY_PLAYLIST_IMG_URL", "https://files.catbox.moe/2pwrhs.jpg"
 )
 
 
