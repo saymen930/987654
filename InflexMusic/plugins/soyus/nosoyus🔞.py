bae3e85530801097f80dbe1febe00273d6fa9d1e
Saymen930 <115408305+saymen930@users.noreply.github.com>
2025-10-31 23:14:22 +0400

    Update nosoyus🔞.py

--- InflexMusic/plugins/soyus/nosoyus🔞.py
+++ InflexMusic/plugins/soyus/nosoyus🔞.py
@@ -4,15 +4,21 @@ from InflexMusic import app
 
 qadağan_sözlər = [
     "sik", "sikdir", "peyser", "peysər", "oğras", "qəhbə", "qehbe", "anavi",
-    "Bacivi", "sikim", "dalbayov", "blət", "blet", 
-    "Pox", "varyox", "doğduğu", "bicbala", "bicok", "ble", "blə", "pesi", "cındır", "cindir", "ogras", "nənəvi", "seks", "sikis"
+    "bacivi", "sikim", "dalbayov", "blət", "blet", "qələt",
+    "pox", "heyvan", "varyox", "doğduğu", "bicbala", "bicok", "ble", "blə", "pesi",
+    "cındır", "cindir", "ogras", "nənəvi", "seks", "sikis"
 ]
 
 @app.on_message(filters.text & filters.group)
 async def check_and_delete(client: Client, message: Message):
     lower_text = message.text.lower()
+
+    # Mesajı sözlərə bölürük (nöqtə, vergül və s. nəzərə alınmasın deyə)
+    import re
+    sözlər = re.findall(r"\b\w+\b", lower_text)
+
     for soz in qadağan_sözlər:
-        if soz in lower_text:
+        if soz in sözlər:  # Yalnız tam uyğun gələn söz varsa
             try:
                 await message.delete()
                 print(f"Silindi: {message.text}")
@@ -21,10 +27,9 @@ async def check_and_delete(client: Client, message: Message):
                     name = f"@{username}"
                 else:
                     name = message.from_user.first_name or "İstifadəçi"
-                
-                warning_text = f"{name} qrupumuzda argo verici kəlmə işlətmək qadağandır ⛔"
+
+                warning_text = f"{name}, qrupumuzda argo ifadə işlətmək qadağandır ⛔"
                 await message.reply(warning_text)
             except Exception as e:
                 print(f"Silmək mümkün olmadı: {e}")
             break
-            
